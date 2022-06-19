aa67eb02dbb5c88dfb74c5814d7fd4c7e417901a
Prince Carl Velasco <princevelasco16@gmail.com>
2022-06-19 22:31:03 +0800

    refactor: change package name

--- src/main/java/com/princevelasco/platform/CodeSharingPlatformApplication.java
+++ src/main/java/com/princevelasco/platform/CodeSharingPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.example.platform;
+package com.princevelasco.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/princevelasco/platform/CodeSharingPlatformApplicationTests.java
+++ src/test/java/com/princevelasco/platform/CodeSharingPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.platform;
+package com.princevelasco.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
